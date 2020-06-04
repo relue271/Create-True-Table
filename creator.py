c2e18a2fc26e9e29f82873135eb01f09af943f9f
Euler G Gonçalves <goncalves.euler@gmail.com>
2020-06-04 09:46:12 -0300

    Create creator.py

---
+++ creator.py
@@ -0,0 +1,13 @@
+""""
+Criador de tabela verdades
+
+entrada é um numero inteiro
+saida um arquivo txt com todas as possibilidades da tabela verdade
+
+ex:creator(2)
+2.txt
+0 0
+0 1
+1 0
+1 1
+""""
