b000ade7d9008098e5692c847089a608ae273d5d
Euler G Gonçalves <goncalves.euler@gmail.com>
2022-01-12 10:57:19 -0300

    Update creator.py
    versao 2

--- creator.py
+++ creator.py
@@ -1,13 +1,8 @@
-""""
-Criador de tabela verdades
 
-entrada é um numero inteiro
-saida um arquivo txt com todas as possibilidades da tabela verdade
+def main():
+  print("in: ")
+  
+  
+  
 
-ex:creator(2)
-2.txt
-0 0
-0 1
-1 0
-1 1
-""""
+main()
